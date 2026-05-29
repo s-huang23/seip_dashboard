49a6e648732e88a99d2706f193a5ea1583d613d8
s-huang23 <siqinhuang23@gmail.com>
2026-05-29 10:15:33 -0500

    Fix seaborn import alias

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
-import seaborn as sb
+import seaborn as sns
 
 st.set_page_config(
     page_title="Penguins Dashboard",
